82c5f245b8cde0f4842b3a0224a5ea0d74b77c06
Devmustroc <elmustapha.abourar@outlook.com>
2025-02-20 19:56:36 +0100

    add Dockerfile and configure global environment variables with ConfigModule

--- backend/auth-service/src/app.module.ts
+++ backend/auth-service/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+      // Import the ConfigModule
+      ConfigModule.forRoot({
+        isGlobal: true, // set to true to make the config module global,
+        envFilePath: '.env',
+      }),
+      PrismaModule,
+      AuthModule,
+      UserModule,
+      MfaModule,
+      RabbitMQModule
+  ],
 })
 export class AppModule {}
